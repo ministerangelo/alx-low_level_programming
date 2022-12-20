754a9895ab705904924c4f6ddab4128a48e83726
Stanley Angelo <stangelo1234@gmail.com>
2022-12-20 08:26:28 -0800

    kingdom

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
@@ -19,8 +17,8 @@ void more_numbers(void)
 	{
 		if (j >= 10)
 
-			_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+		_putchar(j / 10 + '0');
+		_putchar(j % 10 + '0');
 
 	}
 		_putchar('\n');
