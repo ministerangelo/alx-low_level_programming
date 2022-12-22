f54a7729be2e9705cec771fae514fce6f039b3d0
Stanley Angelo <stangelo1234@gmail.com>
2022-12-22 11:27:30 -0800

    kong

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,11 +2,8 @@
 
 /**
  * _strcmp - compare string values
- *
  * @s1: input value
- *
  * @s2: input value
- *
  * Return: s1[i] - s2[i]
  *
  */
@@ -16,22 +13,20 @@ int _strcmp(char *s1, char *s2)
 {
 
 		int i;
-		
+
 		i = 0;
 
 			while (s1[i] != '\0' && s2[i] != '\0')
 
-		{
 
-			if (s1[i] != s2[i])
-		{
+			{
+				
+				if (s1[i] != s2[i])
+			}
+
+
 														return (s1[i] - s2[i]);
-
-		}
-
 				i++;
-		}
-
 					return (0);
 }
 
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,6 @@
 
 /**
  * cap_string - Capitalizes all words of a string.
- *
  * @str: The string to be capitalized.
  * Return: A pointer to the changed string.
  *
@@ -16,16 +15,12 @@ char *cap_string(char *str)
 
 		int index = 0;
 
-
-
 		while (str[index])
 
-			while (!(str[index] >= 'a' && str[index] <= 'z')
+			while (!(str[index] >= 'a' && str[index] <= 'z'))
 					index++;
-
-
-
-		if (str[index - 1] == ' ' |
+		{
+		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
 			str[index - 1] == ',' ||
@@ -37,10 +32,12 @@ char *cap_string(char *str)
 			str[index - 1] == ')' ||
 			str[index - 1] == '{' ||
 			str[index - 1] == '}' ||
-																																index == 0;
+			
+			index == 0);
+
 			str[index] -= 32;
 												index++;
-			}
+		}
 			return (str);
 }
 
