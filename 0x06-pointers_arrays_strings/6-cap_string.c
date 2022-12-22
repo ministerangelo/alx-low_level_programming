5e50a0cd8073a4ec6cce0ce95d6c0a8d733eb25f
Stanley Angelo <stangelo1234@gmail.com>
2022-12-22 12:16:19 -0800

    news

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,8 @@ char *cap_string(char *str)
 		while (str[index])
 
 			while (!(str[index] >= 'a' && str[index] <= 'z'))
-					index++;
+
+				index++;
 		{
 		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
@@ -32,7 +33,7 @@ char *cap_string(char *str)
 			str[index - 1] == ')' ||
 			str[index - 1] == '{' ||
 			str[index - 1] == '}' ||
-			
+
 			index == 0)
 			str[index] -= 32;
 												index++;
