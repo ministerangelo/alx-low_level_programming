95ae8102325c6b60ef406fe91bbd76a2503c7eda
Angelo Stanley <stangelo1234@gmail.com>
2023-01-27 21:10:27 -0800

    king

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,21 +1,16 @@
 #include <stdlib.h>
-
 #include <string.h>
-
 #include "lists.h"
-
 
 
 /**
  * add_node_end - adds a new node at the end of a linked list
  *
- *   * @head: double pointer to the list_t list
+ * @head: double pointer to the list_t list
  *
- *    * @str: string to put in the new node
+ * @str: string to put in the new node
  *
- *     *
- *
- *      * Return: address of the new element, or NULL if it failed
+ * Return: address of the new element, or NULL if it failed
  *
  */
 
